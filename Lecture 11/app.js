e0dc04db3de529262e683689bc70cd652869ac4d
Dareen Alsulami <DareenAlsulami11@gmail.com>
2024-03-04 20:37:25 +0300

    modified:   Lecture 11/app.js 	modified:   Lecture 11/index.html 	new file:   module2-solution/README.txt

--- Lecture 11/app.js
+++ Lecture 11/app.js
@@ -2,11 +2,18 @@
 'use strict'
 angular.module('Msgapp',[])
 .controller('MsgController', MsgFunction);
-MsgFunction.$inject=['$scope'];
+MsgFunction.$inject=['$scope', '$filter'];
 
-function MsgFunction($scope){
+function MsgFunction($scope, $filter){
   $scope.stateOfBeing="angry";
+  $scope.foodCost=2;
  
+  $scope.saymsg= function SayMessage(){
+    var msg = 'FIFI likes to eat peoples hands !';
+    var output= $filter('uppercase')(msg);
+
+    return output;
+  };
   $scope.feedFifi= function(){
     $scope.stateOfBeing="happy";
   };
